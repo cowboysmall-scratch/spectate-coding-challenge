fca34497422d7650711ae768ab8107fc97879318
Jerry Kiely <jerry@cowboysmall.com>
2023-03-21 15:40:12 +0000

    made some slight changes to formatting to conform to pep8

--- spectate/entity.py
+++ spectate/entity.py
@@ -2,7 +2,7 @@
 """
 
       My initial approach was to implement a lightweight ORM framework, but I
-    decided against it as I don't feel like I have enough years left in my
+    decided against it as I didn't feel like I had enough years left in my
     life.
 
       The below would have contained meta information about tables, and various
@@ -20,17 +20,17 @@ class Entity:
 
     def __init__(self, name, row):
         self.name = name
-        self.row  = row
+        self.row = row
 
 
 class Row:
 
     def __init__(self, *columns):
-        self.columns      = columns
+        self.columns = columns
         self.column_names = [c.column_name for c in columns]
         self.column_types = [c.column_type for c in columns]
-        self.column_type  = {c.column_name: c.column_type for c in columns}
-        self.nullable     = {c.column_name: c.nullable    for c in columns}
+        self.column_type = {c.column_name: c.column_type for c in columns}
+        self.nullable = {c.column_name: c.nullable for c in columns}
 
     def get_columns(self):
         return ["id"] + self.column_names
@@ -44,8 +44,7 @@ class Row:
 
 class Column:
 
-    def __init__(self, column_name, column_type, nullable = False):
+    def __init__(self, column_name, column_type, nullable=False):
         self.column_name = column_name
         self.column_type = column_type
-        self.nullable    = nullable
-
+        self.nullable = nullable
